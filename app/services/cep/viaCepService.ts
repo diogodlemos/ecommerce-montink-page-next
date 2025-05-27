0b7a4a49978f48708b5a67aa41a785a3b6a936a3
Diogo Lemos <diogodrummond@hotmail.com>
2025-05-26 23:47:30 -0300

    service para a api viacep e interface Address

---
+++ app/services/cep/viaCepService.ts
@@ -0,0 +1,15 @@
+import axios from "axios"
+
+const findCepToValidation = async (cep: string) => {
+    try {
+        const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`)
+        console.log(response);
+        return response;
+    } catch (error) {
+        console.log("error", error)
+    }
+}
+
+export default {
+    findCepToValidation
+}
